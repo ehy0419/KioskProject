ff2d3390d96556444e7d30ece493e4c8b50c639e
ehy0419 <2hy0419@naver.com>
2025-07-22 10:45:56 +0900

    작동 확인 x

--- src/main/java/org/example/kiosk/Main.java
+++ src/main/java/org/example/kiosk/Main.java
@@ -1,12 +1,51 @@
 package org.example.kiosk;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         /// 클래스 정의
-        // 시작 지점이 되는 클래스
+        // 프로그램 시작 지점이 되는 클래스
 
         Kiosk kiosk = new Kiosk();
 
+        // 작동 확인
+        Scanner scanner = new Scanner(System.in);
+
+        // 메뉴아이템 표시
+        while (true) {
+            System.out.println("[ SHAKESHACK MENU ]");
+            System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+            System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+            System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+            System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+            System.out.println("0. 종료           | 종료");
+
+            System.out.print("원하는 메뉴를 선택하세요(번호): ");
+            int burgerNumber = scanner.nextInt();
+
+            if (burgerNumber == 0) {
+                System.out.println("메뉴 선택을 종료합니다.");
+            }
+            break;  // 무한 루프 조심!
+        }
+
+        // 메뉴
+        switch (burgerNumber) {
+            case 1:
+                System.out.println();
+                break;
+            case 2:
+                System.out.println();
+                break;
+            case 3:
+                System.out.println();
+                break;
+            case 4:
+                System.out.println();
+                break;
+
+        }
     }
 }
--- src/main/java/org/example/kiosk/MenuItem.java
+++ src/main/java/org/example/kiosk/MenuItem.java
@@ -3,15 +3,16 @@ package org.example.kiosk;
 public class MenuItem {
 
     /// 클래스 정의
-    // 세부 메뉴 속성 가지는 클래스
+    // 메뉴 항목 클래스.
+    // - 세부 메뉴 속성 가지는 클래스
     // 내용
     // 햄버거의 이름, 가격 설명
     // 예시 : ShackBurger, 6.9, 토마토, 양상추, 쉑소스가 토핑된 치즈버거
     // new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거")
 
     MenuItem(String name, double price, String description) {
-        this.name = name;
-        this.price = price;
-        this.description = description;
+//        this.name = name;
+//        this.price = price;
+//        this.description = description;
     }
 }
