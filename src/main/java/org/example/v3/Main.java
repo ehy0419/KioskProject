e66a8a21c6d007cf464275fd219ec55ab7d198be
ehy0419 <2hy0419@naver.com>
2025-07-23 22:07:34 +0900

    lv3 하다가 안 되서 lv3 처음으로 수정

--- src/main/java/org/example/v3/Main.java
+++ src/main/java/org/example/v3/Main.java
@@ -1,4 +1,26 @@
 package org.example.v3;
 
 public class Main {
+
+    ///  lv3 작업 순서
+    /// Kiosk 클래스 생성
+    //menuItems 리스트가 필드로 존재
+    //start() 메서드에서 main 함수에서 입력과 반복문 로직 관리
+    /// Main 클래스 단순화
+    //List<MenuItem> menuItems 는 Kiosk 클래스 생성자를 통해 값을 할당
+
+    ///  무슨 말이지...
+    //Kiosk 객체를 생성하고 사용하는 main 함수에서 객체를 생성할 때 값을 넘겨줍니다. -
+
+    /// 클래스 정의
+    // 프로그램 시작 지점이 되는 클래스
+    // 메뉴 목록 생성 후 kisok 실행
+
+    public static void main(String[] args) {
+
+        // 메뉴 목록
+        // Kiosk 객체 생성 후 start() 함수 생성
+        // start 메서드 추가
+        // Kiosk 클래스에도 같은 생성자 만들기!
+    }
 }
