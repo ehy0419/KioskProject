26994f75f6bade600971fb51b2bb64344f8d944c
ehy0419 <2hy0419@naver.com>
2025-07-23 22:24:54 +0900

    lv3. 메뉴 목록

--- src/main/java/org/example/v3/Main.java
+++ src/main/java/org/example/v3/Main.java
@@ -1,5 +1,8 @@
 package org.example.v3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     ///  lv3 작업 순서
@@ -19,6 +22,20 @@ public class Main {
     public static void main(String[] args) {
 
         // 메뉴 목록
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("ShackBurger",6.9,"토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Cheeseburger", 6.9, "토마토, 양상추, 쉑소스가 토핑된포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
+        // 메뉴 목록 직접 타이핑
+        // lv2 main에서 복붙하면
+//        List<org.example.lv2.MenuItem> menuItems = new ArrayList<>();
+//        menuItems.add(new org.example.lv2.MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+//        menuItems.add(new org.example.lv2.MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+//        menuItems.add(new org.example.lv2.MenuItem("Cheeseburger", 6.9, "토마토, 양상추, 쉑소스가 토핑된포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+//        menuItems.add(new org.example.lv2.MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
         // Kiosk 객체 생성 후 start() 함수 생성
         // start 메서드 추가
         // Kiosk 클래스에도 같은 생성자 만들기!
--- src/main/java/org/example/v3/MenuItem.java
+++ src/main/java/org/example/v3/MenuItem.java
@@ -1,4 +1,30 @@
 package org.example.v3;
 
 public class MenuItem {
+
+    private final String name;
+    private final double price;
+    private final String description;
+
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    // 메뉴 이름을 불러오는 메서드
+    public String getName() {
+        return name;
+    }
+
+    // 메뉴 가격을 불러오는 메서드
+    public double getPrice() {
+        return price;
+    }
+
+    // 메뉴 설명을 불러오는 메서드
+    public String getDescription() {
+        return description;
+    }
+
 }
